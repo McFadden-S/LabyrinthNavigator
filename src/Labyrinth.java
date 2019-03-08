3f9e6b5a9f35c6e0f7c24c5d26aa15be6722a891
McFadden-S <shaefromhome@shaw.ca>
2019-03-07 19:40:51 -0600

    Initial commit

---
+++ src/Labyrinth.java
@@ -0,0 +1,32 @@
+/* **********************************************************
+ * Programmer:  McFadden
+ * Class:	CS40S
+ * 
+ * Assignment:	A4
+ *
+ * Description:	This class will hold a map of labyrinth, as well as calculate
+ *              number of paths
+ * 
+ * *************************************************************
+ */
+ 
+ // import files here as needed
+ 
+ 
+ public class Labyrinth
+ {  // begin class
+ 	
+ 	// *********** class constants **********
+ 	// ********** instance variable **********
+ 	
+        int[][] lab;
+        String map;
+        int path;
+     
+ 	// ********** constructors ***********
+ 	
+ 	// ********** accessors **********
+ 	
+ 	// ********** mutators **********
+ 
+ }  // end class
---
+++ src/Main.java
@@ -0,0 +1,53 @@
+// **********************************************************************
+// Programmer:	McFadden
+// Class:	CS40S
+//
+// Assignment:	A4
+//
+// Description:	This program will read int size array and non passable addresses
+//              then will print map and number of passes
+//
+//
+// Input:	The input will come from a fill, first line dimensions, second
+//              line will be number of non passable addresses n, next n line
+//              are the addresses
+//
+//  Output:	will print out string map of labyrinth then the number of paths
+// ***********************************************************************
+
+import javax.swing.*;
+import java.text.DecimalFormat;
+
+public class Main
+{  // begin class
+	public static void main(String args[])
+	{  // begin main
+	// ***** declaration of constants *****
+	
+	// ***** declaration of variables *****
+	
+	// ***** create objects *****
+		
+		
+	// ***** create input stream *****
+	
+		
+		
+	// ***** Print Banner *****
+	
+		System.out.println("**********************************");
+		System.out.println("NAME:        Shae McFadden");
+		System.out.println("Class:       CS40S");
+		System.out.println("Assignment:  A4");
+		System.out.println("**********************************");
+		
+	// ***** main body *****
+	
+
+
+	// ***** closing message *****
+	
+		System.out.println("end of processing");
+	
+	}  // end main	
+}  // end class
