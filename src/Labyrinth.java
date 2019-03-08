bc30e386c3dd5d27986f39f9f3314e367f6a17fb
McFadden-S <shaefromhome@shaw.ca>
2019-03-07 20:05:06 -0600

    V0.1
    Class
    - added constructor
    - added method to build array
    - added method to create map

--- src/Labyrinth.java
+++ src/Labyrinth.java
@@ -25,8 +25,66 @@
      
  	// ********** constructors ***********
  	
- 	// ********** accessors **********
+        /*
+        Purpose: Constructor for the Labyrinth which will automatically
+        calculate basic information
+        Input: a 2D array of the blocked addresses
+        */
+        public Labyrinth(int row, int column, int[][] blocks){
+         
+            lab = builder(row, column, blocks);
+            map = mapMaker(lab);
+            //path = pathfinder(lab); 
+        }
+        
+ 	// ********** Calculators **********
  	
- 	// ********** mutators **********
+        /*
+        Purpose: method to create the labyrinth array with blocked addresses
+        Input: number(int) of rows and columns as well as the blocked addresses (int[][])
+        Output: the labyrinth array (int[][])
+        */
+        private int[][] builder(int row, int column, int[][] block){
+            int[][] l = new int[row][column];
+            
+            for(int i = 0; i<block.length; i++){
+                l[block[i][0]][block[i][1]] = -1;
+            }//end of for loop
+            
+            return l;
+        }//end of builder
+        
+        /*
+        Purpose: Make a string map of the labyrinth for output
+        Input: labyrinth array(int[][])
+        Output: String Map of Labyrinth
+        */
+        private String mapMaker(int[][] l){
+            String map = "";
+            
+            for(int i = 0; i<l.length; i++){
+                for(int j = 0; j<l[0].length; j++){
+                    if(l[i][j] == -1){
+                        map += "m ";
+                    }//end of if monster
+                    else if(i==0&&j==0){
+                        map += "s ";
+                    }//end of else if entrance
+                    else if(i==l.length-1&&j==l[0].length-1){
+                        map += "e ";
+                    }//end of else if exit
+                    else {
+                        map += "o ";
+                    }//end of else open
+                }//end of inner for loop
+                map += "\n";
+            }//end of outter for loop
+            
+            return map;
+        }//end of mapMaker
+        
+        
+        
+ 	// ********** IO **********
  
  }  // end class
